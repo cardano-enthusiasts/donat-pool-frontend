b48b923b938c8d38c0e3f08220c6643ab25aa0ad
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-02-27 14:06:00 +0300

    added create protocol button and changed offchain commit

--- src/layouts/Home/Home.tsx
+++ src/layouts/Home/Home.tsx
@@ -1,5 +1,8 @@
+import { toast } from 'react-toastify';
+
 import Button from 'shared/components/Button/Button';
 import Cards from 'shared/components/Cards/Cards';
+import { useOffchain } from 'shared/hooks/useOffchain';
 
 import {
   Description,
@@ -13,6 +16,30 @@ import {
 } from './Home.styled';
 
 const Home = () => {
+  const offchain = useOffchain();
+  const handleStartProtocolSuccess = (params) => {
+    console.log(params);
+
+    toast.success('Protocol was started');
+  };
+
+  const handleStartProtocolError = () => {
+    toast.error('Start protocol error');
+  };
+
+  const startProtocolParams = {
+    minAmountParam: 50000000,
+    maxAmountParam: 1000000000,
+    minDurationParam: 100,
+    maxDurationParam: 1000,
+    protocolFeeParam: 10,
+  };
+  const handleStartProtocolClick = () => {
+    offchain.startProtocol(handleStartProtocolSuccess)(
+      handleStartProtocolError
+    )(startProtocolParams)();
+  };
+
   return (
     <Wrapper>
       <MainScreen>
@@ -26,7 +53,9 @@ const Home = () => {
               elit
             </Description>
             <MainButtons>
-              <Button theme="filled">Create project</Button>
+              <Button theme="filled" onClick={handleStartProtocolClick}>
+                start protocol
+              </Button>
               <Button theme="bordered">Learn more</Button>
             </MainButtons>
           </IntroInner>
