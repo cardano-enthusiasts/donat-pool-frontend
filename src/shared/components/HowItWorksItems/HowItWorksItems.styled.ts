1acd2536603de805a79c3c3182dd2e3d3fea073e
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-06-27 16:15:46 +0300

    added animation to how it works

--- src/shared/components/HowItWorksItems/HowItWorksItems.styled.ts
+++ src/shared/components/HowItWorksItems/HowItWorksItems.styled.ts
@@ -4,16 +4,41 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 60px;
-  @media (max-width: 1100px) {
+  @media (max-width: 1360px) {
     gap: 30px;
   }
 `;
 
 const Item = styled.div`
+  height: 66px;
+  &:hover {
+    > div:first-child {
+      transform: rotateX(90deg) translateZ(33px);
+    }
+    > div:last-child {
+      transform: rotateX(0) translateZ(10px) translateY(-66px);
+    }
+  }
+  @media (max-width: 730px) {
+    height: auto;
+  }
+`;
+
+const Main = styled.div`
   font-weight: bold;
   font-size: 48px;
   line-height: 130%;
   color: ${({ theme }) => theme.colors.black};
+  transition: transform 0.5s linear;
+  transition-delay: 0.1s;
+  height: 66px;
+  @media (max-width: 1360px) {
+    font-size: 34px;
+  }
+  @media (max-width: 730px) {
+    transition: none;
+    height: auto;
+  }
   @media (max-width: 600px) {
     font-size: 32px;
   }
@@ -22,4 +47,38 @@ const Item = styled.div`
   }
 `;
 
-export { Wrapper, Item };
+const Secondary = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  transform: rotateX(-90deg) translateZ(-40px);
+  height: 66px;
+  transition: transform 0.5s linear;
+  transition-delay: 0.1s;
+  padding: 0 40px;
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: bold;
+  font-size: 20px;
+  background-color: ${({ theme }) => theme.colors.red};
+  @media (max-width: 1200px) {
+    padding: 0 20px;
+  }
+  @media (max-width: 730px) {
+    transform: none;
+    background-color: transparent;
+    padding: 0;
+    height: auto;
+    color: ${({ theme }) => theme.colors.black};
+  }
+`;
+
+const Tertiary = styled.div`
+  color: ${({ theme }) => theme.colors.yellow};
+  font-size: 16px;
+  font-weight: normal;
+  @media (max-width: 730px) {
+    color: ${({ theme }) => theme.colors.black};
+  }
+`;
+
+export { Wrapper, Item, Main, Secondary, Tertiary };
--- src/shared/components/HowItWorksItems/HowItWorksItems.tsx
+++ src/shared/components/HowItWorksItems/HowItWorksItems.tsx
@@ -1,17 +1,48 @@
-import { Item, Wrapper } from './HowItWorksItems.styled';
+import {
+  Main,
+  Secondary,
+  Item,
+  Wrapper,
+  Tertiary,
+} from './HowItWorksItems.styled';
 
 const HowItWorksItems = () => {
   const items = [
-    'Easily register with a single click',
-    'Create a project',
-    'Flexible donation options',
-    'Withdraw the amount to your wallet',
+    {
+      title: 'Easily register with a single click',
+      description: 'Create your account with just connecting your lightwallet',
+      descriptionMinor: 'Works only with Nami wallet',
+      id: 0,
+    },
+    {
+      title: 'Create a project',
+      description:
+        'Create a project and share a link to the project page with your followers/contributors.',
+      id: 1,
+    },
+    {
+      title: 'Flexible donation options',
+      description: 'Withdraw the amount to your wallet',
+      id: 2,
+    },
+    {
+      title: 'Withdraw the amount to your wallet',
+      description:
+        'Withdraw the funds with just a single click and a small service fee after the project campaign is completed',
+      id: 3,
+    },
   ];
 
   return (
     <Wrapper>
-      {items.map((item) => (
-        <Item key={item}>{item}</Item>
+      {items.map(({ title, description, descriptionMinor, id }) => (
+        <Item key={id}>
+          <Main>{title}</Main>
+          <Secondary>
+            {description}
+            {descriptionMinor && <Tertiary>{descriptionMinor}</Tertiary>}
+          </Secondary>
+        </Item>
       ))}
     </Wrapper>
   );
