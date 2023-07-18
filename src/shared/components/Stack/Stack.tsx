da16527f09329f12ec149ace1f3a48c8651f41a0
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-07-18 17:35:00 +0300

    Revert "changed imgs for stack"
    This reverts commit d6eabfa46472729e01f7c84283d9636dc6023ace.

--- src/shared/components/Stack/Stack.tsx
+++ src/shared/components/Stack/Stack.tsx
@@ -2,29 +2,17 @@ import { useEffect, useRef, useState } from 'react';
 
 import { useWindowScroll } from 'shared/helpers/hooks/useWindowScroll';
 
-import { DocLink, Img, Items, Title, Wrapper } from './Stack.styled';
+import { Img, Items, Title, Wrapper } from './Stack.styled';
 import { Button } from '../.';
 
 const Stack = () => {
   const ref = useRef<HTMLDivElement>(null);
   const [isActive, setIsActive] = useState(false);
   const windowScroll = useWindowScroll();
-  const stack: Array<{ id: string; src: string; href: string }> = [
-    {
-      id: 'plutarch',
-      src: '/img/plutarch.svg',
-      href: 'https://github.com/Plutonomicon/plutarch-plutus',
-    },
-    {
-      id: 'cardano-transaction-lib',
-      src: '/img/cardano-transaction-lib.svg',
-      href: 'https://github.com/Plutonomicon/cardano-transaction-lib',
-    },
-    {
-      id: 'react',
-      src: '/img/react.svg',
-      href: 'https://react.dev/',
-    },
+  const stack: Array<{ id: string; src: string }> = [
+    { id: 'plutarch', src: '/img/plutarch.svg' },
+    { id: 'cardano-transaction-lib', src: '/img/cardano-transaction-lib.svg' },
+    { id: 'react', src: '/img/react.svg' },
   ];
   useEffect(() => {
     if (ref.current) {
@@ -38,10 +26,8 @@ const Stack = () => {
     <Wrapper>
       <Title>Our stack</Title>
       <Items ref={ref}>
-        {stack.map(({ id, src, href }) => (
-          <DocLink target="_blank" rel="noreferrer" href={href} key={id}>
-            <Img src={src} alt={id} isActive={isActive} />
-          </DocLink>
+        {stack.map(({ id, src }) => (
+          <Img key={id} src={src} alt={id} isActive={isActive} />
         ))}
       </Items>
 
