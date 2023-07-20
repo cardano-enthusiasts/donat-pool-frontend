ed356be20f57b2447fd8b729b409423dd4c75a93
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-07-20 09:16:17 +0300

    set common attributes

--- src/shared/components/Input/Input.tsx
+++ src/shared/components/Input/Input.tsx
@@ -25,36 +25,33 @@ const Input = ({
   min = 0,
   step = 1,
 }: Props) => {
+  const attributes = {
+    value,
+    onChange,
+    placeholder,
+    maxLength,
+    error,
+    fontColor,
+    disabled: isDisabled,
+  };
   return (
     <Wrapper>
       <Title>{children}</Title>
       <InputContainer hint={hint}>
         {multiline ? (
           <StyledTextArea
-            value={value}
-            onChange={onChange}
+            {...attributes}
             {...{ 'data-type': dataAttr }}
-            disabled={isDisabled}
-            placeholder={placeholder}
-            maxLength={maxLength}
             rows={rows}
-            error={error}
-            fontColor={fontColor}
           />
         ) : (
           <StyledInput
             type={type}
-            value={value}
-            onChange={onChange}
+            {...attributes}
             {...{ 'data-type': dataAttr }}
-            disabled={isDisabled}
-            placeholder={placeholder}
-            maxLength={maxLength}
-            error={error}
             onWheel={(event) => {
               event.currentTarget.blur();
             }}
-            fontColor={fontColor}
             min={type === 'number' ? min : undefined}
             step={type === 'number' ? step : undefined}
           />
