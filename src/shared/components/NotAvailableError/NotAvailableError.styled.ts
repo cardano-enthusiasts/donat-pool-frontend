736a02f99dffcdd87434185082d3ad76f762d064
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-07-26 10:08:16 +0300

    added not available page

--- src/shared/components/NotAvailableError/NotAvailableError.styled.ts
+++ src/shared/components/NotAvailableError/NotAvailableError.styled.ts
@@ -8,23 +8,43 @@ const Container = styled.form`
   justify-content: space-between;
   width: 100%;
   height: 100vh;
+  min-height: 670px;
   background: ${({ theme }) => theme.colors.red};
 
   margin: 0 auto;
 `;
 
-const Content = styled.div``;
+const Inner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100% - 100px);
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 600px;
+  padding: 40px 40px 60px 40px;
+`;
 
 const Title = styled.h1`
   ${h1};
   color: ${({ theme }) => theme.colors.white};
+  margin-bottom: 24px;
+  text-align: center;
 `;
 
-const Img = styled.img``;
+const Img = styled.img`
+  max-width: 140px;
+  margin-bottom: 40px;
+`;
 
 const Description = styled.div`
   color: ${({ theme }) => theme.colors.black};
   text-align: center;
+  margin-bottom: 32px;
 `;
 
-export { Container, Content, Title, Img, Description };
+export { Container, Content, Title, Img, Description, Inner };
--- src/shared/components/NotAvailableError/NotAvailableError.tsx
+++ src/shared/components/NotAvailableError/NotAvailableError.tsx
@@ -3,6 +3,7 @@ import {
   Content,
   Description,
   Img,
+  Inner,
   Title,
 } from './NotAvailableError.styled';
 import { Button, Waves } from '..';
@@ -10,21 +11,25 @@ import { Button, Waves } from '..';
 const NotAvailableError = () => {
   return (
     <Container>
-      <Content>
-        <Title>Wallet is not available </Title>
-        <Img src="img/sad-cat-with-purple-border.svg" />
-        <Description>
-          Please install Nami wallet in a suitable browser (Chrome, Brave)
-        </Description>
-        <Button
-          href="https://namiwallet.io/"
-          themeType="double-bordered"
-          primaryColor="blue"
-          tertiaryColor="red"
-        >
-          Download App
-        </Button>
-      </Content>
+      <Inner>
+        <Content>
+          <Title>Wallet is not available </Title>
+          <Img src="img/sad-cat-with-purple-border.svg" />
+          <Description>
+            Please install Nami wallet in a suitable browser (Chrome, Brave)
+          </Description>
+          <Button
+            href="https://namiwallet.io/"
+            themeType="double-bordered"
+            primaryColor="blue"
+            tertiaryColor="red"
+            width="100%"
+          >
+            Download App
+          </Button>
+        </Content>
+      </Inner>
+
       <Waves />
     </Container>
   );
