6a5c872b02d8d116397811e6857e5affeec05fae
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-07-27 09:19:55 +0300

    added shared hooks folder

--- src/shared/helpers/hooks/index.ts
+++ src/shared/helpers/hooks/index.ts
@@ -3,7 +3,7 @@ export { useGetAllFundraisings } from './offchain/info/useGetAllFundraisings';
 export { useGetUserFundraisings } from './offchain/info/useGetUserFundraisings';
 export { useCreateFundraising } from './offchain/fundraising/useCreateFundraising';
 export { useDonate } from './offchain/fundraising/useDonate';
-export { useGetAppInfo } from './offchain/protocol/useGetAppInfo';
+export { useGetAppInfo } from './offchain/shared/useGetAppInfo';
 export { useUpdateProtocol } from './offchain/protocol/useUpdateProtocol';
 export { useReceiveFunds } from './offchain/fundraising/useReceiveFunds';
 export { useStartProtocol } from './offchain/protocol/useStartProtocol';
