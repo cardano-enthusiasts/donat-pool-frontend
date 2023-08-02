43f37285eb865f225c646c781f7c22f6e20d6024
alagunoff <lagunov.artamon@yandex.ru>
2023-08-02 18:05:39 +0300

    updated offchain project

--- src/shared/helpers/hooks/offchain/useOffchain.ts
+++ src/shared/helpers/hooks/offchain/useOffchain.ts
@@ -1,14 +1,5 @@
-import { useEffect, useState } from 'react';
-
 const useOffchain = () => {
-  const [offchain, setOffchain] = useState<Window['offchain']>();
-  useEffect(() => {
-    if (window.offchain) {
-      window.offchain.then(setOffchain);
-    }
-  }, []);
-
-  return offchain;
+  return window.donatPool;
 };
 
 export { useOffchain };
