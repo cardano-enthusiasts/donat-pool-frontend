e13677b7209d0def5484e260a51b8fccbca73aa1
alagunoff <lagunov.artamon@yandex.ru>
2023-08-03 11:34:57 +0300

    added copying necessary files from offchain repo in build time

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {};
+const path = require('node:path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: './node_modules/offchain/dist',
+            to: path.resolve(__dirname, 'public/offchain'),
+          },
+        ],
+      }),
+    );
+
+    return config;
+  },
+};
