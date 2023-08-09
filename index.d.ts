b0b27dfcd0dfc8a12ba4fa249d52edcdeac5cf53
alagunoff <lagunov.artamon@yandex.ru>
2023-08-09 11:17:56 +0300

    fix eslint errors

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-import { type Fundraising } from '@/shared/types';
-import { type FundraisingData } from '@/shared/types/common';
+import type { Fundraising } from '@/shared/types';
+import type { FundraisingData } from '@/shared/types/common';
 
 interface WalletParameters {
   wallet: 'Nami';
