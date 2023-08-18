68e6649b779e5960ce09a09c3ccc46da653615c4
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-08-18 09:52:38 +0300

    fixed logo and footer

--- src/shared/components/Logo/Logo.tsx
+++ src/shared/components/Logo/Logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { ROUTES } from '@/shared/constants';
 
 const Logo = () => {
-  const classes = 'max-[450px]:max-w-50 max-[450px]:block';
+  const classes = 'max-[450px]:opacity-0';
   return (
     <Link href={ROUTES.home} className={classes}>
       <Image src="/img/logo.svg" width={283} height={40} alt="logo" className={classes} />
