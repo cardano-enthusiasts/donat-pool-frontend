4e9e031f0e1f87ae66b763ffa1b8a77f5ec4779b
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-08-21 11:36:05 +0300

    deleted extra js

--- src/layouts/Project/Project.tsx
+++ src/layouts/Project/Project.tsx
@@ -1,19 +1,15 @@
 import cn from 'classnames';
 
-import { useWindowSize } from '@/shared/hooks';
-
 import styles from './Project.module.css';
 import type { Props } from './types';
 
 const Project = ({ onPreviousPageClick, previousPageTitle, title, children }: Props) => {
-  const { width } = useWindowSize();
-
   return (
     <div className="relative flex justify-center">
       <div className="w-full max-w-[620px]">
         <div className="mb-[60px] flex justify-between max-xl:justify-start max-lg:mb-8">
           <div className={cn(styles.previous, 'text-blue')} onClick={onPreviousPageClick}>
-            {width > 1200 ? previousPageTitle : ''}
+            <div className="max-xl:hidden">{previousPageTitle}</div>
           </div>
           <h1 className="overflow-hidden text-ellipsis">{title}</h1>
         </div>
