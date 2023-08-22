094a13d01123115a75d16bf008e7199b4df08214
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-08-22 15:30:34 +0300

    edited how it works texts

--- src/shared/components/HowItWorks/HowItWorks.tsx
+++ src/shared/components/HowItWorks/HowItWorks.tsx
@@ -8,13 +8,10 @@ const HowItWorks = () => {
   return (
     <>
       <div className="mb-15 flex flex-col gap-15 max-xl:mb-[1.875rem] max-xl:gap-[1.875rem]">
-        {ITEMS.map(({ title, description, descriptionMinor, id }) => (
+        {ITEMS.map(({ title, description, id }) => (
           <div className={styles.item} key={id}>
             <div className={cn('text-black', styles.main)}>{title}</div>
-            <div className={cn('bg-red text-white max-md:text-black', styles.secondary)}>
-              {description}
-              {descriptionMinor && <div className="not-italic text-yellow max-md:text-black">{descriptionMinor}</div>}
-            </div>
+            <div className={cn('bg-red text-white max-md:text-black', styles.secondary)}>{description}</div>
           </div>
         ))}
       </div>
