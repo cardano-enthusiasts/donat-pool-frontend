1fea63a6249611d749cb956ca9b010878a4e99f3
alagunoff <lagunov.artamon@yandex.ru>
2023-08-22 10:23:14 +0300

    removed auto setting for line endings

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,5 @@ const alloyPrettierConfig = require('eslint-config-alloy/.prettierrc');
 
 module.exports = {
   ...alloyPrettierConfig,
-  endOfLine: 'auto',
   plugins: ['prettier-plugin-tailwindcss'],
 };
