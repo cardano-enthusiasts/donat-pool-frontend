dafd68d2f9730826eb3e98f16edbd2eebabe95b8
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-08-23 15:36:53 +0300

    deleted target type

--- src/shared/components/Footer/constants.ts
+++ src/shared/components/Footer/constants.ts
@@ -3,7 +3,6 @@ import { ROUTES } from '@/shared/constants';
 const LINKS: Array<{
   title: string;
   href: string;
-  target?: React.HTMLAttributeAnchorTarget;
 }> = [
   { title: 'Home', href: ROUTES.home },
   { title: 'FAQ', href: ROUTES.faq },
