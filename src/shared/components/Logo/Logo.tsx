87e05c79b121e269b9501c7e2477d83286687c06
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-08-23 09:18:07 +0300

    changed logo styles

--- src/shared/components/Logo/Logo.tsx
+++ src/shared/components/Logo/Logo.tsx
@@ -4,10 +4,9 @@ import Link from 'next/link';
 import { ROUTES } from '@/shared/constants';
 
 const Logo = () => {
-  const classes = 'max-[450px]:opacity-0';
   return (
-    <Link href={ROUTES.home} className={classes}>
-      <Image src="/img/logo.svg" width={283} height={40} alt="logo" className={classes} />
+    <Link href={ROUTES.home} className="max-[450px]:opacity-0">
+      <Image src="/img/logo.svg" width={283} height={40} alt="logo" />
     </Link>
   );
 };
