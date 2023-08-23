80ffd0acc7273ba05d6a2fbae11cabfdaa7c00c3
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-08-23 15:36:31 +0300

    deleted about us on metalamp link

--- src/shared/components/Footer/constants.ts
+++ src/shared/components/Footer/constants.ts
@@ -6,7 +6,6 @@ const LINKS: Array<{
   target?: React.HTMLAttributeAnchorTarget;
 }> = [
   { title: 'Home', href: ROUTES.home },
-  { title: 'About us on MetaLamp', href: ROUTES.mock },
   { title: 'FAQ', href: ROUTES.faq },
   { title: 'Terms of use', href: ROUTES.termsOfUse },
 ];
