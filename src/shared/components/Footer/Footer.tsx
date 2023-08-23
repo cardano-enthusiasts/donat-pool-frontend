f8fedae190447efce26ebb87c13a19a4ed09e895
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-08-23 14:41:34 +0300

    deleted target param

--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -23,8 +23,8 @@ const Footer = ({ backgroundColor = 'blue' }: Props) => {
           <div className="flex items-center gap-32 max-lg:flex-col max-lg:gap-10">
             <Socials />
             <div className=" flex gap-20 font-rammetto-one text-white max-xl:flex-col max-xl:gap-4 max-lg:items-center">
-              {LINKS.map(({ title, href, target }) => (
-                <Link href={href} target={target} key={title} className="text-[0.9375rem] leading-snug">
+              {LINKS.map(({ title, href }) => (
+                <Link href={href} key={title} className="text-[0.9375rem] leading-snug">
                   {title}
                 </Link>
               ))}
