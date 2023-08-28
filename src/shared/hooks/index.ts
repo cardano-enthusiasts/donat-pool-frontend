0a984b7ed64c3fcacc01804fad5eb26e62220a81
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-08-28 12:47:30 +0300

    deleted extra use client

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,5 +1,3 @@
-'use client';
-
 export { default as useAppDispatch } from './useAppDispatch';
 export { default as useAppSelector } from './useAppSelector';
 export { default as useCreateFundraising } from './useCreateFundraising';
--- src/shared/hooks/useFundraisings.ts
+++ src/shared/hooks/useFundraisings.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { useCallback, useEffect } from 'react';
 
 import { setRequestStatus, setFundraisings, setError } from '@/redux/slices/getAllFundraisings';
