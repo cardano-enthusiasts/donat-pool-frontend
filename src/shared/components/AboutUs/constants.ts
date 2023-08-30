5ee84a99a19713b9f58a83ba8f3aebc60c4beb65
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-08-30 12:03:56 +0300

    fixed xss sizes and created common row styles

--- src/shared/components/AboutUs/constants.ts
+++ src/shared/components/AboutUs/constants.ts
@@ -6,15 +6,17 @@ import svetlana from '@public/img/svetlana.png';
 
 import { Photo } from './types';
 
+const COMMON_ROW_STYLES = 'max-xxs:h-[56.25rem] max-1.5xl:h-[75rem] max-xl:h-[62.5rem] max-md:h-[43.75rem]';
+
 const STYLES = {
-  kate: 'absolute left-[-3.125rem] top-[5.125rem] z-[2] h-auto w-[21.875rem] shrink-0 max-1.5xl:left-0 max-xl:top-[5.6875rem] max-xl:w-[16.4375rem] max-md:top-[6.0625rem] max-md:w-[10.9375rem]',
+  kate: 'absolute left-[-3.125rem] top-[5.125rem] z-[2] h-auto w-[21.875rem] shrink-0 max-1.5xl:left-0 max-xl:top-[5.6875rem] max-xl:w-[16.4375rem] max-md:top-[6.0625rem] max-md:w-[10.9375rem] max-xxs:top-80',
   oksana:
-    'absolute left-[8%] top-[-1.375rem] z-[1] h-auto w-[26.25rem] shrink-0 max-1.5xl:left-0 max-1.5xl:right-0 max-1.5xl:mx-auto max-xl:top-[0.8125rem] max-xl:w-[19.6875rem] max-md:top-[2.8125rem] max-md:w-[13.125rem] max-[380px]:top-[-13.75rem]',
-  olga: 'w-[23.125rem] absolute left-[26%] top-[6.9375rem] z-[1] h-auto shrink-0 max-1.5xl:left-auto max-1.5xl:right-0 max-xl:w-[17.375rem] max-md:w-[11.5625rem]',
+    'absolute left-[8%] top-[-1.375rem] z-[1] h-auto w-[26.25rem] shrink-0 max-1.5xl:left-0 max-1.5xl:right-0 max-1.5xl:mx-auto max-xl:top-[0.8125rem] max-xl:w-[19.6875rem] max-md:top-[2.8125rem] max-md:w-[13.125rem] max-xxs:top-[0]',
+  olga: 'w-[23.125rem] absolute left-[26%] top-[6.9375rem] z-[1] h-auto shrink-0 max-1.5xl:left-auto max-1.5xl:right-0 max-xl:w-[17.375rem] max-md:w-[11.5625rem] max-xxs:top-[20.9375rem]',
   svetlana:
-    'absolute left-1/2 top-[8.0625rem] z-[3] h-auto w-[21.875rem] shrink-0 max-1.5xl:bottom-0 max-1.5xl:left-0 max-1.5xl:top-auto max-xl:w-[16.4375rem] max-md:w-[10.9375rem]',
+    'absolute left-1/2 top-[8.0625rem] z-[3] h-auto w-[21.875rem] shrink-0 max-1.5xl:bottom-0 max-1.5xl:left-0 max-1.5xl:top-auto max-xl:w-[16.4375rem] max-md:w-[10.9375rem] max-xxs:top-[44.0625rem]',
   mariya:
-    'absolute right-[-1.875rem] z-[1] h-auto w-[400px] shrink-0 max-1.5xl:bottom-[3rem] max-1.5xl:right-0 max-xl:bottom-[2.25rem] max-xl:w-[18.75rem] max-md:bottom-[1.5rem] max-md:w-[12.5rem]',
+    'absolute right-[-1.875rem] z-[1] h-auto w-[25rem] shrink-0 max-1.5xl:bottom-[3rem] max-1.5xl:right-0 max-xl:bottom-[2.25rem] max-xl:w-[18.75rem] max-md:bottom-[1.5rem] max-md:w-[12.5rem] max-xxs:top-[40rem]',
 };
 
 const PHOTOS: Photo[] = [
@@ -25,4 +27,4 @@ const PHOTOS: Photo[] = [
   { src: mariya, className: STYLES.mariya, alt: 'mariya photo', isFirstRow: false },
 ];
 
-export { PHOTOS };
+export { PHOTOS, COMMON_ROW_STYLES };
--- src/shared/components/AboutUs/index.tsx
+++ src/shared/components/AboutUs/index.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 
-import { PHOTOS } from './constants';
+import { COMMON_ROW_STYLES, PHOTOS } from './constants';
 import { Stack } from '../.';
 
 const AboutUs = () => {
@@ -20,34 +20,32 @@ const AboutUs = () => {
     <div className="mb-36 max-xl:mb-10 max-md:mb-0">
       <div className="relative flex flex-col items-center">
         <div
-          className="relative
-          mb-[8.75rem]
-          mt-15
-          flex
-          h-[37.5rem]
-          w-full
-          max-w-[68.125rem]
-          max-1.5xl:h-[75rem]
-          max-1.5xl:max-w-[50.75rem]
-          max-xl:mb-12
-          max-xl:mt-8
-          max-xl:h-[62.5rem]
-          max-xl:max-w-[39rem]
-          max-md:h-[43.75rem]
-          max-md:max-w-[25.1875rem]"
+          className={`${COMMON_ROW_STYLES}
+            relative
+            mb-[8.75rem]
+            mt-15
+            flex
+            h-[37.5rem]
+            w-full
+            max-w-[68.125rem]
+            max-1.5xl:max-w-[50.75rem]
+            max-xl:mb-12
+            max-xl:mt-8
+            max-xl:max-w-[39rem]
+            max-md:max-w-[25rem]
+          `}
         >
           {getPhotos(true)}
           <div
-            className="max-1.5xl:relative
+            className={`${COMMON_ROW_STYLES}
+              max-1.5xl:relative
               max-1.5xl:mx-auto
               max-1.5xl:my-0
-              max-1.5xl:h-[75rem]
               max-1.5xl:w-full
               max-1.5xl:max-w-[39.375rem]
-              max-xl:h-[62.5rem]
               max-xl:max-w-[29.375rem]
-              max-md:h-[43.75rem]
-              max-md:max-w-[19.6875rem]"
+              max-md:max-w-[19.6875rem]
+            `}
           >
             {getPhotos(false)}
           </div>
