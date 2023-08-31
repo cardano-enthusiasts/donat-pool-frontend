d722ebac74eb4d486238c9e6129a2e4664f70cbf
alagunoff <lagunov.artamon@yandex.ru>
2023-08-31 19:16:55 +0300

    removed unnecessary setting line height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ const metadata: Metadata = {
 function Layout({ children }: React.PropsWithChildren) {
   return (
     <html
-      className={`${rammettoOneFont.variable} ${microsoftYaHeiFont.variable} bg-white font-microsoft-ya-hei text-[16px]/6 text-black`}
+      className={`${rammettoOneFont.variable} ${microsoftYaHeiFont.variable} bg-white font-microsoft-ya-hei text-[16px] text-black`}
       lang="en"
     >
       <body>
