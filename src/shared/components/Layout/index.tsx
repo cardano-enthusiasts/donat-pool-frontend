f75bdb36c0b8eae05f1894624cbaa74314e5fd61
alagunoff <lagunov.artamon@yandex.ru>
2023-09-04 16:28:42 +0300

    refactored Layout

--- src/shared/components/Layout/index.tsx
+++ src/shared/components/Layout/index.tsx
@@ -2,9 +2,9 @@ import { Header, Footer } from '@/shared/components';
 
 function Layout({ children }: React.PropsWithChildren) {
   return (
-    <div className="mx-auto flex min-h-screen max-w-screen-fhd flex-col">
+    <div className="mx-auto flex min-h-screen min-w-[20rem] max-w-screen-fhd flex-col">
       <Header />
-      <main className="mx-auto w-full max-w-screen-2xl flex-grow px-20 pb-40 pt-20 max-md:p-10">{children}</main>
+      <main className="w-full flex-grow px-20 pb-40 pt-20 max-md:p-10">{children}</main>
       <Footer />
     </div>
   );
