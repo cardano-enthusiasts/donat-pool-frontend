8253d291013a517ac3264e34b26bf22a2766f4b1
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-09-12 18:08:05 +0300

    added axios error

--- src/redux/slices/allDonatPools/thunk/index.ts
+++ src/redux/slices/allDonatPools/thunk/index.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { isAxiosError } from 'axios';
 
 import api from '@/shared/api';
 import type { DonatPool } from '@/shared/types';
@@ -8,7 +9,10 @@ const requestAllDonatPools = createAsyncThunk('/allDonatPools', async function (
     const response = await api.get<DonatPool[]>('fundraising-api/all-projects/');
     return response.data;
   } catch (error) {
-    return rejectWithValue(error);
+    if (isAxiosError(error)) {
+      return rejectWithValue(error.message);
+    }
+    return rejectWithValue(JSON.stringify(error));
   }
 });
 
