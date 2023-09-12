07d66fb0d54e530a4634e36e27b8c4174ca8d550
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-09-12 10:04:59 +0300

    fixed image import

--- src/shared/components/Loading/constants.tsx
+++ src/shared/components/Loading/constants.tsx
@@ -1,8 +1,8 @@
-import CatImage0 from '@public/img/loading-cat-0.svg';
-import CatImage1 from '@public/img/loading-cat-1.svg';
-import CatImage2 from '@public/img/loading-cat-2.svg';
-import CatImage3 from '@public/img/loading-cat-3.svg';
-import CatImage4 from '@public/img/loading-cat-4.svg';
+import CatImage0 from '@public/images/loading-cat-0.svg';
+import CatImage1 from '@public/images/loading-cat-1.svg';
+import CatImage2 from '@public/images/loading-cat-2.svg';
+import CatImage3 from '@public/images/loading-cat-3.svg';
+import CatImage4 from '@public/images/loading-cat-4.svg';
 
 const images = [
   <CatImage0 key="0" />,
