9a5e3113d96434c942f91d80ae7f45292775e864
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-09-29 09:20:34 +0300

    changed description

--- src/shared/components/TitleAndDescription/index.tsx
+++ src/shared/components/TitleAndDescription/index.tsx
@@ -20,24 +20,18 @@ function TitleAndDescription({ active }: Props) {
       <BigLogoImage className="mb-[1.875rem] max-w-[48.125rem]" />
       <div
         className="mb-10
-          flex
           w-full
           max-w-[62.5rem]
-          flex-col
-          text-8xl/none
+          font-microsoft-ya-hei
+          text-[3.375rem]/tight
+          font-bold
           text-green
           delay-200
-          max-xl:w-[70%]
-          max-xl:max-w-[31.875rem]
-          max-xl:text-5xl
-          max-sm:w-full
-          max-sm:max-w-[20rem]
-          max-sm:text-3xl"
+          max-md:text-3xl"
       >
-        <div>Give a little,</div>
-        <div className="self-end">help a lot</div>
+        Crowdfunding on Cardano blockchain for everyone
       </div>
-      <div className="mb-[5.625rem] max-lg:mb-12">
+      <div className="mb-[5.625rem] ml-6 max-lg:mb-12">
         <QuaternaryLink animation="continuous" href={ROUTES.donatPools}>
           Start
           <br />
